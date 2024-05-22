a02774d0048e4a1e282342876dc05df8d3685128
O1or1n <knaduevv@gmail.com>
2024-05-23 01:46:08 +0300

    commit5

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ def answer_on_command(message):
     markup.row(button_basket)
     markup.row(button_review)
     bot.send_message(message.chat.id, f'Привет {message.from_user.username}', reply_markup=markup)
+
+@bot.message_handler()
+def lp(message):
+    global cap, lat, isOtiv
+    if message.text == 'Меню':
+        isOtiv = False
+        markup = types.InlineKeyboardMarkup()
+        cappuccino = types.InlineKeyboardButton('Капучино', callback_data='cappuccino')
+        latte = types.InlineKeyboardButton('Латте', callback_data='latte')
+        markup.row(cappuccino, latte)
+        bot.reply_to(message, 'Меню', reply_markup=markup)
