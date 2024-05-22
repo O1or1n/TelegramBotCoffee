f78c61ba5535a4afd20c3eaae2de0792c5c10e21
O1or1n <knaduevv@gmail.com>
2024-05-23 01:42:15 +0300

    commit3

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ bot = telebot.TeleBot('7069938356:AAHMhXXPD0l0Wwf23dwk3I-yV2gAXZAlHYc')#кири
 lat = 0
 cap = 0
 isOtiv = False
+
+@bot.message_handler(commands=['start'])
+def answer_on_command(message):
+    markup = types.ReplyKeyboardMarkup()
+    button_menu = types.KeyboardButton('Меню')
+    button_payment = types.KeyboardButton('Оплата')
+    button_basket = types.KeyboardButton('Корзина')
+    button_review = types.KeyboardButton('Отзыв')
